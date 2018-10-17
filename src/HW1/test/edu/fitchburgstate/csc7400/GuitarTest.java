201225b9135b01c5f6fee1a95ba49c5bfd06ba1c
adityagandi1203 <43256465+adityagandi1203@users.noreply.github.com>
2018-10-16 20:01:06 -0400

    Update and rename src/HW1/test/GuitarTest.java to src/HW1/test/edu/fitchburgstate/csc7400/GuitarTest.java

--- src/HW1/test/edu/fitchburgstate/csc7400/GuitarTest.java
+++ src/HW1/test/edu/fitchburgstate/csc7400/GuitarTest.java
@@ -1,13 +1,14 @@
-package HW1;
 
+package HW1.test.edu.fitchburgstate.csc7400;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import HW1.Guitar;
 
-class GuitarTest {
+/*test the guitar class by pulling each parameter of the constructor*/
+class GuitarTest
+{
 	
 	Guitar testGuitar;
 
@@ -80,3 +81,4 @@ class GuitarTest {
 		String returned = this.testGuitar.getTopWood();
 		assertEquals(expected, returned, String.format("Top wood %s != %s", expected, returned));
 	}
+}
